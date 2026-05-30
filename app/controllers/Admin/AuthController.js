876a3d673736787c1de0c046f972d1137ec5d195
Gourav Kumar <gni.gourav@gmail.com>
2026-05-30 10:17:26 +0530

    change2qsadszxqqascxzeacsz

--- app/controllers/Admin/AuthController.js
+++ app/controllers/Admin/AuthController.js
@@ -1,31 +1,41 @@
 app.controller("AuthController", function ($scope, apiService) {
-  $scope.credentials = {};
 
-  $scope.login = function (loginForm) {
-    if (loginForm.$invalid) {
-      return;
-    }
+    $scope.credentials = {};
 
-    const apiData = {
-      phone: $scope.credentials.phone,
-      password: $scope.credentials.password,
+    $scope.login = function (loginForm) {
+
+        if (loginForm.$invalid) {
+            return;
+        }
+
+        const apiData = {
+            phone: $scope.credentials.phone,
+            password: $scope.credentials.password
+        };
+
+        apiService.login(apiData)
+            .then(function (response) {
+
+                if (response.data.success) {
+
+                    localStorage.setItem("token", response.data.token);
+
+                    $scope.success = response.data.message;
+                    alert($scope.success);
+
+                } else {
+
+                    $scope.error = response.data.message;
+                    alert($scope.error);
+                }
+
+            })
+            .catch(function (error) {
+
+                $scope.error = error.data?.message || "Something went wrong";
+                alert($scope.error);
+
+            });
     };
 
-    apiService
-      .login(apiData)
-      .then(function (response) {
-        if (response.data.success) {
-          localStorage.setItem("token", response.data.token);
-          $scope.success = response.data.message;
-        alert($scope.success);
-        } else {
-          $scope.error = response.data.message;
-          alert($scope.error);
-        }
-      })
-      .catch(function (error) {
-        $scope.error = error.data?.message;
-        alert($scope.error);
-      });
-  };
 });
